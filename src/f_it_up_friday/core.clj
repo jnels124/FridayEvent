3f1abd9f9d6b22b3addaff11f7903b5f5de6e3bf
dviramontes <dviramontes@levelsbeyond.com>
2015-06-26 12:27:06 -0600

    init

--- src/f_it_up_friday/core.clj
+++ src/f_it_up_friday/core.clj
@@ -1,6 +1,38 @@
-(ns f-it-up-friday.core)
+(ns f-it-up-friday.core
+  (:require [clojure.tools.cli :refer [cli]]
+            [clojure.java.io :refer [writer reader file]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def version-number 0.1)
+
+(def help
+  ["-h" "--help" "Print this help"
+    :flag false])
+
+(def cwd
+  (System/getProperty "user.dir"))
+
+(def version
+  ["-v" "--version" "Print Version"
+   :default false])
+
+(def create
+  ["-c" "--create" "Create Controller"
+   :parse-fn #()])
+
+
+(defn write-file [name ext]
+  (writer (file (str cwd "/" name ext))))
+
+(defn -main [& args]
+
+  (let [[opts args banner]
+        (cli args version help create)]
+
+    (println opts)
+    (cond
+      (contains? opts :help) (print banner)
+      (contains? opts :version) (str "v" version-number))
+      (contains? opts :create) (write-file "foo" )))
+
+
