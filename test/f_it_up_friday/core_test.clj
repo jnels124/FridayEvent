585ad441bcf6558c632fb2f401ef73ee06e39c30
Jesse <jnels124@msudenver.edu>
2015-06-26 08:17:53 -0600

    Prepare Project

---
+++ test/f_it_up_friday/core_test.clj
@@ -0,0 +1,7 @@
+(ns f-it-up-friday.core-test
+  (:require [clojure.test :refer :all]
+            [f-it-up-friday.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
