bfd125d9d164e1ba40246868851eda426a92d52b
Jesse <jnels124@msudenver.edu>
2015-06-28 23:23:13 -0600

    cleanup and bindToController string

--- src/f_it_up_friday/angular/templates.clj
+++ src/f_it_up_friday/angular/templates.clj
@@ -20,52 +20,54 @@
 (defn module-string
   [cl-opts, type]
   (let [mod-opts (clojure.string/split (cl-opts (keyword type)) #",")]
-    (str "angular.module('"
-         (cl-opts :module-name)
-         "')."
-         type
-         "('"
-         (first mod-opts)
-         "'"
-         (injection-string (apply str (interpose ", " (map #(str "'" % "'") (rest mod-opts))))
-                           (apply str (interpose ", " (rest mod-opts)))))))
-
-(defn create-controller
-  [cl-opts]
-  (io/write-content (cl-opts :path)
-                    (str (cl-opts :file-name) "-controller.js")
-                    (module-string cl-opts "controller")))
+    (str
+      "'use strict';\n"
+      "angular.module('"
+      (cl-opts :module-name)
+      "')."
+      type
+      "('"
+      (first mod-opts)
+      "'"
+      (injection-string (apply str (interpose ", " (map #(str "'" % "'") (rest mod-opts))))
+                        (apply str (interpose ", " (rest mod-opts)))))))
 
 (defn scope-entry
   [entry]
   (str (indent) (first entry) ": '" (last entry) "'"))
 
 (defn directive-scope-def
-  [scope]
+  [scope, bind-to-controller]
   (if (not (empty? scope))
-    (str (indent)
-         "scope: {\n"
-         (indent)
-         (->> (clojure.string/split scope #",")
-              (map #(-> (clojure.string/split % #":") scope-entry))
-              (interpose (str ",\n" (indent)))
-              (apply str))
-         "\n"
-         (indent)
-         "}")
+    (let [scope-str (->> (clojure.string/split scope #",")
+                         (map #(-> (clojure.string/split % #":") scope-entry))
+                         (interpose (str ",\n" (indent)))
+                         (apply str))]
+      (str (indent)
+           ;;will move this to bind-to-controller-def. Also needs to handle when bindToController: true
+           (if bind-to-controller
+             (str  "bindToController: {\n"
+                  (indent) scope-str ",\n" (indent) "},\n" (indent)))
+           "scope: {\n"
+           (indent)
+           scope-str
+           "\n"
+           (indent)
+           "}"))
+
     (str (indent) "scope: true")))
 
 (defn directive-controller-def
-  [cl-opts]
-  (if (cl-opts :controller)
-    (str (indent) "controller: '" (first (clojure.string/split (cl-opts :controller) #",")) "',\n")))
+  [controller]
+  (if controller
+    (str (indent) "controller: '" (first (clojure.string/split controller #",")) "',\n")))
 
 (defn directive-bind-to-controller-def
-  [cl-opts]
-  (if (cl-opts :bind-to-controller)
+  [ctrl-as]
+  (if ctrl-as
     (str (indent)
          "controllerAs: '"
-         (cl-opts :bind-to-controller)
+         ctrl-as
          "',\n")))
 
 (defn template-path
@@ -79,10 +81,16 @@
 (defn directive-template-def
   [cl-opts]
   (if (cl-opts :include-html-template)
-    (str (indent) "templateUrl: '" (template-path (cl-opts :module-name)
-                                                  (clojure.string/split (cl-opts :path) #"/"))
-         "/"
-         (cl-opts :file-name) "-template.html,\n")))
+    (let [template-file (str (cl-opts :file-name)  "-template.html")
+          path (or (cl-opts :path) "./")]
+      (io/write-content path
+                        template-file
+                        "")
+      (str (indent) "templateUrl: '" (template-path (cl-opts :module-name)
+                                                    (clojure.string/split path #"/"))
+           "/"
+           template-file
+           "',\n"))))
 
 (defn directive-link-def
   [include-link]
@@ -100,8 +108,7 @@
               (map #(str "'" % "'"))
               (interpose ", ")
               (apply str))
-         "],\n"
-         )))
+         "],\n")))
 
 ;; TODO: Implement args to control restrict
 (defn create-directive
@@ -114,12 +121,21 @@
                   "restrict: 'E',\n"
                   (directive-template-def cl-opts)
                   (directive-required-def (cl-opts :require))
-                  (directive-controller-def cl-opts)
-                  (directive-bind-to-controller-def cl-opts)
-                  (directive-scope-def (cl-opts :scope))
+                  (directive-controller-def (cl-opts :controller))
+                  (directive-bind-to-controller-def (cl-opts :bind-to-controller))
+                  (directive-scope-def (cl-opts :scope) (cl-opts :bind-to-controller))
                   (directive-link-def (cl-opts :link-fctn))
                   "\n"
                   "};")]
     (io/write-content (cl-opts :path)
                       (str (cl-opts :file-name) "-directive.js")
-                      (clojure.string/replace (module-string cl-opts "directive") "{}" (str "{\n   " dir-str "\n}")))))
+                      (clojure.string/replace (module-string cl-opts "directive") "{}" (str "{\n   " dir-str "\n}")))
+    (swap! level - (deref level))))
+
+
+(defn create-controller
+  [cl-opts]
+  (io/write-content (cl-opts :path)
+                    (str (cl-opts :file-name) "-controller.js")
+                    (module-string cl-opts "controller"))
+  (swap! level - (deref level)))
