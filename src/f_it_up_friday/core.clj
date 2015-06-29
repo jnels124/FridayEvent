134e301e784e60c946b92932efe7a80bca848360
Jesse <jnels124@msudenver.edu>
2015-06-28 23:23:35 -0600

    cleanup

--- src/f_it_up_friday/core.clj
+++ src/f_it_up_friday/core.clj
@@ -43,17 +43,17 @@
   [["-c" "--create" :flag false]])
 
 (def angular-options
-  [["-a" "--add-methods" "Expects comma sepearated string of method + args. i.e fctn1[arg1 arg2], fctn2[arg1]"]
-   ["-b" "--[no-]bind-to-controller" "Binds all values passed to scope on the controller"]
+  [["-a" "--add-methods" "Expects comma sepearated string of method + args. i.e fctn1[arg1 arg2], fctn2[arg1]" :flag false]
+   ["-b" "--[no-]bind-to-controller" "Binds all values passed to scope on the controller. Takes the controlleras name for arg" :flag false]
    ["-c" "--controller" "Specifies controller with injections. i.e. controllername,inj1,inj2" :flag false]
-   ["-d" "--[no-]directive" "Specifies directive. If controller is specified to be created, this will be the directive controller" :flag false]
-   ["-h" "--[no-]include-html-template" "Creates a html template file."]
-   ["-l" "--[no-]link-fctn" "Flag to determine if link function is included for directive" :flag true :default true]
-   ["-m" "--module-name" ""]
-   ["-f" "--file-name" "Specifies the base name for files"]
-   ["-p" "--path" "Specify path string. If no path is provided, files will be placed in project root"]
-   ["-r" "--require" "Comma sepearted list of required directievs"]
-   ["-s" "--scope" "Comma separated list of scope var name and type. i.e var1:&,var2:=some,var3:@"]
+   ["-d" "--[no-]directive" "Specifies directive with injections. i.e. directivename,inj1,inj2 If controller is specified to be created, this will be the directive controller" :flag false]
+   ["-i" "--[no-]include-html-template" "Creates a html template file." :flag true]
+   ["-l" "--[no-]link-fctn" "Flag to determine if link function is included for directive" :flag true :default false]
+   ["-m" "--module-name" "" :flag false]
+   ["-f" "--file-name" "Specifies the base name for files" :flag false]
+   ["-p" "--path" "Specify path string. If no path is provided, files will be placed in project root" :flag false]
+   ["-r" "--require" "Comma sepearted list of required directievs" :flag false]
+   ["-s" "--scope" "Comma separated list of scope var name and type. i.e var1:&,var2:=some,var3:@" :flag false]
    ["-t" "--[no-]create-test" "Creates a test(s) if controller and/or directive is created" :flag true :default true]])
 
 (defn missing-required? [opts framework]
